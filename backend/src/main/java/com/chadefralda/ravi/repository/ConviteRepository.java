ef38fa9b1b4fdddcdb49a879095ddb4cf887485a
Alvarezpro87 <alvareznuvem@gmail.com>
2025-08-05 14:24:42 -0300

    alteração no nome da pasta

--- backend/src/main/java/com/chadefralda/ravi/repository/ConviteRepository.java
+++ backend/src/main/java/com/chadefralda/ravi/repository/ConviteRepository.java
@@ -5,5 +5,5 @@ import com.chadefralda.ravi.model.Convite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConviteRepository extends JpaRepository<Convite, Long> {
-    // depois podemos criar métodos customizados se precisar
+
 }
